66a5318921dfad0446caaad124d79e17fb2bd000
James Coyle <jamesrichardcoyle@gmail.com>
2020-02-07 14:31:13 +0000

    Actually access the module properly

--- lib/polyfilled-dialog.vue
+++ lib/polyfilled-dialog.vue
@@ -7,7 +7,7 @@ export default {
 	created() {
 		if (typeof window.HTMLDialogElement === 'undefined') {
 			import('dialog-polyfill').then((polyfill) => {
-				polyfill.registerDialog(this.$el)
+				polyfill.default.registerDialog(this.$el)
 			})
 		}
 	},
